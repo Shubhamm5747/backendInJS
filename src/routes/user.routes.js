8a98a07e555805f5d36b7138fd537f325aefd66d
Shubhamm5747 <ShubhamK5747@gmail.com>
2025-10-02 14:00:49 +0530

    added controller of getUserChannelProfile plus added an agreggate pipeline for the same in that controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,16 +1,32 @@
 import { Router } from "express";
 import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js"
-import { Verifyjwt } from "../middlewares/auth.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 
 const router = Router()
 
-router.route("/register").post(registerUser)
-
+router.route("/register").post(
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+)
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(Verifyjwt, logoutUser) //added a middleware named Verifyjwt here
+router.route("/logout").post(verifyJWT,  logoutUser) //using required middlewares too as per our need
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+router.route("/current-user").get(verifyJWT, getCurrentUser)
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
 
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage)
 export default router;
